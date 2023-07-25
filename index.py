663da6988e927b53f0ad127891a0de8ca83fdba9
Shubham-K-G <sh.shubham.garg@gmail.com>
2023-07-25 23:55:10 +0530

    first commit

---
+++ index.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import csv
+import schedule
+import time
+
+
+def job():
+
+    print("Hello World")
+
+schedule.every(30).seconds.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+
+
